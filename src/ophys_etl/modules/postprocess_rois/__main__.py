c107968adcade3691eefde2287220b6d693966c4
Arielle Leon <ariellel@alleninstitute.org>
2023-12-12 15:45:41 -0800

    updating for aind data schema

--- src/ophys_etl/modules/postprocess_rois/__main__.py
+++ src/ophys_etl/modules/postprocess_rois/__main__.py
@@ -5,7 +5,7 @@ from argschema import ArgSchemaParser
 from marshmallow import ValidationError
 import json
 from pathlib import Path
-from aind_data_schema.core.processing import Processing, DataProcess, ProcessName, PipelineProce
+from aind_data_schema.core.processing import Processing, DataProcess, ProcessName, PipelineProcess
 from typing import Union
 from datetime import datetime as dt
 from datetime import timezone as tz
