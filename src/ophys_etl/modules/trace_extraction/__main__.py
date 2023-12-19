675638375ee8b7e6c7c289051bf69422fb423218
Arielle Leon <ariellel@alleninstitute.org>
2023-12-19 13:34:36 -0800

    update trace extraction:

--- src/ophys_etl/modules/trace_extraction/__main__.py
+++ src/ophys_etl/modules/trace_extraction/__main__.py
@@ -3,64 +3,11 @@ import argschema
 from ophys_etl.modules.trace_extraction.utils import extract_traces
 from ophys_etl.modules.trace_extraction.schemas import (
         TraceExtractionInputSchema, TraceExtractionOutputSchema)
-import json
-from pathlib import Path
-from aind_data_schema.core.processing import Processing, DataProcess, ProcessName, PipelineProcess
-from typing import Union
+from aind_data_schema.core.processing import ProcessName
 from datetime import datetime as dt
 from datetime import timezone as tz
-import os
 
-
-def write_output_metadata(
-        metadata: dict,
-        input_fp: Union[str, Path],
-        output_fp: Union[str, Path],
-        start_date_time: dt,
-        ) -> None:
-        """Writes output metadata to plane processing.json
-
-        Parameters
-        ----------
-        metadata: dict
-                parameters from suite2p motion correction
-        input_fp: str
-                path to data input
-        output_fp: str
-                path to data output
-        """
-        with open(metadata['neuropil_mask_file'], "r") as f:
-                metadata = json.load(f)
-        processing = Processing(
-                processing_pipeline=PipelineProcess(
-                processor_full_name="Multplane Ophys Processing Pipeline",
-                pipeline_url="https://codeocean.allenneuraldynamics.org/capsule/5472403/tree",
-                pipeline_version="0.1.0",
-                data_processes=[
-                        DataProcess(
-                        name=ProcessName.VIDEO_ROI_TIMESERIES_EXTRACTION,
-                        software_version=os.getenv("OPHYS_ETL_COMMIT_SHA"),
-                        start_date_time=start_date_time,  # TODO: Add actual dt
-                        end_date_time=dt.now(tz.utc),  # TODO: Add actual dt
-                        input_location=str(input_fp),
-                        output_location=str(output_fp),
-                        code_url=(os.getenv("OPHYS_ETL_URL")),
-                        parameters=metadata,
-                        )
-                ],
-                )
-        )
-        print(f"Output filepath: {output_fp}")
-        with open(Path(output_fp).parent.parent.parent / "processing.json", "r") as f:
-                proc_data = json.load(f)
-        processing.write_standard_file(output_directory=Path(output_fp).parent.parent.parent)
-        with open(Path(output_fp).parent.parent.parent / "processing.json", "r") as f:
-                dct_data = json.load(f)
-        proc_data["processing_pipeline"]["data_processes"].append(
-                dct_data["processing_pipeline"]["data_processes"][0]
-        )
-        with open(Path(output_fp).parent.parent.parent / "processing.json", "w") as f:
-                json.dump(proc_data, f, indent=4)
+from ophys_etl.utils.processing_metadata import write_output_metadata
 
 class TraceExtraction(argschema.ArgSchemaParser):
     default_schema = TraceExtractionInputSchema
@@ -76,7 +23,8 @@ class TraceExtraction(argschema.ArgSchemaParser):
                 self.args['rois'])
         self.output(output, indent=2)
         write_output_metadata(
-                metadata=output,
+                output,
+                ProcessName.TRACE_EXTRACTION,
                 input_fp = output['neuropil_mask_file'],
                 output_fp = output['neuropil_mask_file'],
                 start_date_time = start_time
