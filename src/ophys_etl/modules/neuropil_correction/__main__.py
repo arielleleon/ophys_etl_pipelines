d3a9ef75dcb8bc92d2bfe6a91af17f05061df7fa
Arielle Leon <ariellel@alleninstitute.org>
2023-12-19 16:51:43 -0800

    fixing bug in neuropil correction

--- src/ophys_etl/modules/neuropil_correction/__main__.py
+++ src/ophys_etl/modules/neuropil_correction/__main__.py
@@ -11,7 +11,7 @@ from aind_data_schema.core.processing import ProcessName
 from datetime import datetime as dt
 from datetime import timezone as tz
 
-from ophys_etl.utils.processing_metadata import write_output_metadata
+from ophys_etl.utils.process_metadata import write_output_metadata
 from ophys_etl.modules.neuropil_correction.schemas import (
     NeuropilCorrectionJobSchema,
     NeuropilCorrectionJobOutputSchema,
